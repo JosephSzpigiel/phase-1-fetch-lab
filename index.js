2ca8e869b6583609716e8c19e680266045b59965
Joseph Szpigiel <josephszpigiel@gmail.com>
2023-09-15 12:51:21 -0400

    lab complete

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function fetchBooks() {
   fetch("https://anapioficeandfire.com/api/books")
   .then((resp) => resp.json())
   .then((json) => renderBooks(json))
+  .then((books => {return books}))
 }
 
 function renderBooks(books) {
